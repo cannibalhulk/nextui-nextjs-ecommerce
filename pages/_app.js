f30c9d45147c0900c55702b73d77e0838117e1f3
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-01-30 00:43:55 +0400

    root page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,61 @@
-import '@/styles/globals.css'
+// 1. import `NextUIProvider` component
+import { NextUIProvider, createTheme } from '@nextui-org/react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export  const lightTheme = createTheme({
+  type: 'light',
+  theme: {
+    colors: {
+      background:"#F5F5F5",
+      text:"black",
+    },
+    breakpoints:{
+      xs: '375px',
+      sm: '960px',
+      md: '1280px',
+      lg: '1400px',
+      xl: '1920px'
+    }
+  }
+})
+
+export const darkTheme = createTheme({
+  type:"dark",
+  theme:{
+    colors:{
+      background:"#1a1a1a",
+      text: "#fff",
+    },
+    breakpoints:{
+      xs: '375px',
+      sm: '960px',
+      md: '1280px',
+      lg: '1400px',
+      xl: '1920px'
+    }
+  }
+})
+
+function MyApp({ Component, pageProps }) {
+  // 2. Use at the root of your app
+  const getLayout = Component.getLayout || ((page) => page)
+  return(
+    <NextThemesProvider
+    defaultTheme="system"
+    attribute="class"
+    value={{
+      light: lightTheme.className,
+      dark: darkTheme.className
+    }}
+    >
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
+  )
+    
+  
 }
+
+export default MyApp
+
