67bbaec8d80b42f43f821c5f3630d7c207092463
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-02-18 19:18:16 +0400

    add badge

--- components/Navbarr.jsx
+++ components/Navbarr.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import { Navbar, Button, Text,Dropdown, Avatar } from '@nextui-org/react'
+import { Navbar, Button, Badge, Text,Dropdown, Avatar } from '@nextui-org/react'
 import { Poppins } from '@next/font/google'
 import { useTheme as useNextTheme } from 'next-themes'
 import { Switch, useTheme } from '@nextui-org/react'
 import { ShoppingCartIcon, UserIcon} from "@heroicons/react/24/solid";
-
+import { useCart } from "../modules/AppContext";
 
 export const poppins = Poppins({
     weight:['400','500','600','700','800','900'],
@@ -14,19 +14,22 @@ export const poppins = Poppins({
 function Navbarr() {
     const { setTheme } = useNextTheme();
     const { isDark, type } = useTheme();
+    const cart = useCart()
     return (
       <>
         <Navbar isBordered variant={"floating"}>
           <Navbar.Brand>
             <Link href="/">
-              <Text h3 color={isDark ? "#fff" : "black"} className={poppins.className} weight="bold">Taku</Text>
+              <Text h3 color={isDark ? "#fff" : "black"} className={poppins.className} weight="bold">Techno</Text>
             </Link>
           </Navbar.Brand>
           <Navbar.Content activeColor={"secondary"}>
             <Navbar.Item>
+              <Badge color={"primary"} content={cart.length} variant="flat">
                 <Button  as={Link} href="/cart" auto >
                   <ShoppingCartIcon width="24"/>
                 </Button>
+              </Badge>
             </Navbar.Item>
             <Navbar.Item>
               <Dropdown>
